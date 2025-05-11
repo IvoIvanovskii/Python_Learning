f35ef1f63fa399d8fdb2871ad37201163a6e2a3a
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-11 18:45:29 +0200

    Create Zadaca6.py

---
+++ First_Class/Zadaca6.py
@@ -0,0 +1,19 @@
+# Simple Calculator (Add, Subtract, Multiply, Divide)
+def Calculator(x,y, operation):
+    if operation == "+":
+        return x + y
+    elif operation == "-":
+        return x - y
+    elif operation == "*":
+        return x * y
+    elif operation == "/":
+        return x / y
+    else:
+        return "Invalid operation"
+    
+    a = int(input("Enter the first number: "))
+    b  = int(input("Enter the second number: "))
+    op = input("Enter the wanted operation (+, -, *, /)")
+    result = Calculator(a, b , op)
+    print(f"Result: {result} ")
+    
