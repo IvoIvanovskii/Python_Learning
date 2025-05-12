ed77fcfd4d3f6bd5c287fe27c640f5c940c4f146
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-12 19:35:18 +0200

    Update Vezbi.py

--- Second_Class/Vezbi.py
+++ Second_Class/Vezbi.py
@@ -1,1 +1,21 @@
-#
+# def OddOrEven(a):
+#     if a % 2 == 0:
+#         return "Paren"
+#     elif a % 2 == 1:
+#         return "Neparen"
+#     else:
+#         return "Brojot e Nevaliden"
+
+# x = int(input("vnesi broj za proverka"))
+# print(OddOrEven(x))
+
+#Faktoriel na 5 
+
+def Faktoriel(n):
+    sum = 1
+    for i in range (1, n+1):
+        sum = sum * i
+    return sum
+
+print
+
