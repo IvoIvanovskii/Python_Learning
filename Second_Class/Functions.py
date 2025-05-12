4a2d830f5170397128f9add6ef9e972a21fabf46
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-12 18:45:16 +0200

    Zadaca za input

--- Second_Class/Functions.py
+++ Second_Class/Functions.py
@@ -4,3 +4,13 @@ x = lambda a,b : a*b
 print(x(5,6))
 
 y=int(input("Vnesete broj: "))
+
+#Za vnesen broj proveri dalie edno ili dvocifren broj
+
+x = int(input("Vnesi broj od 1 do 99: "))
+if x < 10 and x > 0:
+    print("Ednocifren")
+elif x >= 10 and x <= 99:
+    print("dvocifren")
+else:
+    print("Brojot ne e validen")
