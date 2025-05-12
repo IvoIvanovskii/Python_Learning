f53f24970c7b9b7029acc48e0ffd5ef294c2d28d
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-12 18:57:29 +0200

    Update Functions.py

--- Second_Class/Functions.py
+++ Second_Class/Functions.py
@@ -8,9 +8,23 @@ y=int(input("Vnesete broj: "))
 #Za vnesen broj proveri dalie edno ili dvocifren broj
 
 x = int(input("Vnesi broj od 1 do 99: "))
-if x < 10 and x > 0:
+# if x < 10 and x > 0:
+#     print("Ednocifren")
+# elif x >= 10 and x <= 99:
+#     print("dvocifren")
+# else:
+#     print("Brojot ne e validen")
+if 1 <= x < 10:
     print("Ednocifren")
-elif x >= 10 and x <= 99:
-    print("dvocifren")
+elif 10 <= x <= 99:
+    print("Dvocifren")
 else:
-    print("Brojot ne e validen")
+    print("Nevaliden broj")
+
+#Primer Funkcii
+
+def suma(a,b):
+    return a+b
+
+print(suma(10,12))
+
