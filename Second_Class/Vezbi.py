46d1f239c7408d3ec7f383a4ee969c3e5df44728
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-12 20:17:46 +0200

    Update Vezbi.py

--- Second_Class/Vezbi.py
+++ Second_Class/Vezbi.py
@@ -1,4 +1,6 @@
-# ZADACA 1: def OddOrEven(a):
+# ZADACA 1: 
+# def OddOrEven(a):
+
 #     if a % 2 == 0:
 #         return "Paren"
 #     elif a % 2 == 1:
@@ -11,7 +13,8 @@
 
 #---------------------------------------------------------
 
-#ZADACA 2: Faktoriel na daden broj
+#ZADACA 2: 
+# Faktoriel na daden broj
 
 # def Faktoriel(n):
 #     sum = 1
@@ -23,7 +26,8 @@
 
 #------------------------------------------------------------
 
-#ZADACA 3: a,b,c kolku ima deletteli vo opsegot od a do b koi se delat so c
+#ZADACA 3: 
+# a,b,c kolku ima deletteli vo opsegot od a do b koi se delat so c
 
 # count = 0
 # a = int(input("Vnesete broj za a: "))
@@ -38,24 +42,25 @@
 
 #--------------------------------------------------------------
 
+#ZADACA 4:
 # Да се напише програма која што ќе ги испринта
 # сите броеви од 1 до n (n е број внесен од тастатура).
 # доколку бројот се дели со 3 да се испечати "#tri"
 # Доколку се дели со 5 да се испечати "#pet"
 # Доколку се дели со 3 и 5 да се испечати "#TriPet"
 
-n = int(input("Vnesi broj"))
-for i in range (1,n+1):
-    print(i)
+# n = int(input("Vnesi broj"))
+# for i in range (1,n+1):
+#     print(i)
 
-for i in range (1,n+1):
+# for i in range (1,n+1):
     
-    if i % 3 == 0 and i % 5 == 0:
-        print("#TriPet")
-        print("-----------------------")
-    elif i % 3 == 0:
-        print("#Tri")
-        print("------------------------")
-    elif i % 5 == 0:
-        print ("#Pet")
-        print("------------------------")
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("#TriPet")
+#         print("-----------------------")
+#     elif i % 3 == 0:
+#         print("#Tri")
+#         print("------------------------")
+#     elif i % 5 == 0:
+#         print ("#Pet")
+#         print("------------------------")
