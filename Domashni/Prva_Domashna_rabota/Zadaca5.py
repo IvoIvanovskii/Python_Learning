b816056e66ed4864b219e7c466bac3171611b83f
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-21 19:07:21 +0200

    fixes

--- Domashni/Prva_Domashna_rabota/Zadaca5.py
+++ Domashni/Prva_Domashna_rabota/Zadaca5.py
@@ -6,8 +6,12 @@
 
 lista = [23, 65, 12, 99, 34, 87, 3, 56, 78, 45]
 new_list = []
+
 for num in lista:
     if num > 50:
         new_list.append(num)
         
 print("Броевите поголеми од 50 се:", new_list)
+
+# new_List = [x for x in lista if x > 50]
+# print (new_List)
--- Domashni/Prva_Domashna_rabota/Zadaca6.py
+++ Domashni/Prva_Domashna_rabota/Zadaca6.py
@@ -4,19 +4,13 @@
 # го соодветниот ден од неделата.
 
 number = int(input("Внесете број од 1 до 7: "))
-if number == 1:
-    print("Понеделник")
-elif number == 2:
-    print("Вторник")
-elif number == 3:
-    print("Среда")
-elif number == 4:
-    print("Четврток")
-elif number == 5:
-    print("Петок")
-elif number == 6:
-    print("Сабота")
-elif number == 7:
-    print("Недела")
+lista_Denovi = ["Ponedelnik" , "Vtornik", "Sreda", "Chetvrtok", "Petok", "Sabota", "Nedela"]
+
+if 1 <= number <= 7:
+    print(lista_Denovi[number-1])
 else:
-    print("Внесете број помеѓу 1 и 7")
+    print("Izbravte nepostechki broj")
+
+
+
+
