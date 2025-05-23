7e40096d1483f6a589b3c45a15243cf5346be2b1
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-23 18:49:54 +0200

    Update Files.py

--- Fourth_Class/Files.py
+++ Fourth_Class/Files.py
@@ -1,37 +1,37 @@
-try:
-    print(x)
-except:
-    print("There is an error")
+# try:
+#     print(x)
+# except:
+#     print("There is an error")
 
 
-try:
-    print(x)
-except NameError:
-    print("X ne e definirana")
-except:
-    print("imate nekoj drug bug")
+# try:
+#     print(x)
+# except NameError:
+#     print("X ne e definirana")
+# except:
+#     print("imate nekoj drug bug")
 
 
-try:
-    print("Hello")
-except NameError:
-    print("Nema output")
-else:
-    print("se e vo red")
+# try:
+#     print("Hello")
+# except NameError:
+#     print("Nema output")
+# else:
+#     print("se e vo red")
 
 
-try:
-    print(x)
-except:
-    print("Something went wrong")
-finally:
-    print("Hi")
+# try:
+#     print(x)
+# except:
+#     print("Something went wrong")
+# finally:
+#     print("Hi")
 
 
-try:
-    print(10/0)
-except ZeroDivisionError:
-    print("Ne mozhete da delite so 0")
+# try:
+#     print(10/0)
+# except ZeroDivisionError:
+#     print("Ne mozhete da delite so 0")
 
 # Da se vnesi int, i toj int da se deli od 10
 # da se hadnle sluchaite na vnesuvanje razlichni vrednosti od int ili 0
@@ -51,3 +51,4 @@ except ZeroDivisionError:
 
 f = open("C:\\Users\\ivoiv\\OneDrive\\Desktop\\Python\\Fourth_Class\\test.txt")
 print(f.read())
+f.close()
