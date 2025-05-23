1234ec3afa3a6012b27020a753a9e3396820b257
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-23 18:35:04 +0200

    Files

---
+++ Fourth_Class/Files.py
@@ -0,0 +1,48 @@
+try:
+    print(x)
+except:
+    print("There is an error")
+
+
+try:
+    print(x)
+except NameError:
+    print("X ne e definirana")
+except:
+    print("imate nekoj drug bug")
+
+
+try:
+    print("Hello")
+except NameError:
+    print("Nema output")
+else:
+    print("se e vo red")
+
+
+try:
+    print(x)
+except:
+    print("Something went wrong")
+finally:
+    print("Hi")
+
+
+try:
+    print(10/0)
+except ZeroDivisionError:
+    print("Ne mozhete da delite so 0")
+
+# Da se vnesi int, i toj int da se deli od 10
+# da se hadnle sluchaite na vnesuvanje razlichni vrednosti od int ili 0
+
+try:
+    x = int(input("Vnesi eden broj: "))
+    res = 10 / x
+    print(res)
+
+except ValueError:
+    print("Nemate vneseno broj")
+    
+except ZeroDivisionError:
+    print ("Vnesete broj pogolem od 0")
