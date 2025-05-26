efeecb6690f9245c339895d505f129a18cbc0be7
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-26 19:19:37 +0200

    .

--- Domashni/Prva_Domashna_rabota/Vtora_Domashna/Zadaca4.py
+++
@@ -1,9 +0,0 @@
-# Задача 4:
-# File: "info.txt" содржи:
-# “Hello world
-# Python is great
-# This is a test file”
-
-# да се избројат колку редици се во филот.
-
-open(info.txt)
--- Domashni/Vtora_Domashna/Zadaca4.py
+++ Domashni/Vtora_Domashna/Zadaca4.py
@@ -4,12 +4,10 @@
 # Python is great
 # This is a test file”
 
-# да се избројат колку редици се во филот.
 
-import os
-print(os.getcwd())
 
-with open("info.txt", "r") as file:
+
+with open(r"c:\Users\ivoiv\OneDrive\Desktop\Python\Domashni\Vtora_Domashna\info.txt", "r", encoding="utf-8") as file:
 
     lines = file.readlines()
     print(lines)
