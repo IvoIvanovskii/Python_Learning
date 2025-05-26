363bd8f7deccbbb0670e4ebc363ce8a871b9306f
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-26 20:14:43 +0200

    Update Regular_expressions.py

--- Fifth_Class/Regular_expressions.py
+++ Fifth_Class/Regular_expressions.py
@@ -16,7 +16,19 @@ new_text = re.sub(pattern3,"_", text2)
 print(new_text)
 
 text3 = "There are 15 apples and 20 oranges"
-pattern4 = r"\d+"
+pattern4 = r"\d{2}"
 
 match = re.findall(pattern4, text3)
 print(match)
+
+text4 = "Mojot telefonski broj e 078-553-183"
+pattern5 = r"\d{3}-\d{3}-\d{3}"
+match = re.search(pattern5, text4)
+print(match)
+print(match.group())
+
+text5 = "Alice and Bob went to New Yourk"
+pattern6 = r"\b[A-Z][a-zA-Z]*\xb"
+match = re.findall(pattern6, text5)
+print(match)
+
