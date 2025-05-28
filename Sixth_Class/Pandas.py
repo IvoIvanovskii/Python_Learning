76c359e3b5aa7814546ad34a74a69f8d0e7d1dbf
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-05-28 18:48:48 +0200

    csv

--- Sixth_Class/Pandas.py
+++ Sixth_Class/Pandas.py
@@ -1,1 +1,24 @@
 import pandas as pd
+
+dictionary={
+
+    "Ime" : ["Ana", "Angela", "Tea", "Stefan"],
+    "Prezime" : ["Markov", "Markovska", "Stoilkova", "Mirchevski"],
+    "department " : ["IT", "HR", "Finance", "IT"],
+    "e-mail" : ["ana@gmail.com", "Angela@gmail.com", "SOilkova@gmail.com", "Mirchesvski@gmail.com"], 
+    "vozrast" : [19,22,25,35]
+
+}
+
+print(dictionary)
+
+print("--------------------------------------------------------------------------------------------")
+
+df = pd.DataFrame(dictionary)
+print(df)
+
+print("--------------------------------------------------------------------------------------------")
+
+filter = (df["Prezime"] == "Markov") |( df["Prezime"] == "Mirchevski")
+print(filter)
+
