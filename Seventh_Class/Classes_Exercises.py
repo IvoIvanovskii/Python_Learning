2357cc5a2169548fecfc9423b300f78d62ae4050
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-06-04 18:45:50 +0200

    Update Classes_Exercises.py

--- Seventh_Class/Classes_Exercises.py
+++ Seventh_Class/Classes_Exercises.py
@@ -4,9 +4,25 @@ class Company:
         self.location = location
 
 class Employee:
-    def __init__(self, name, position):
+    def __init__(self, name, position, company):
         self.name = name
         self.position = position
-        
+        self.company = company
+
     def description(self):
         print(f"{self.name} raboti")
+
+    def description1(self):
+        print(f"{self.name} raboti kako {self.position} vo kompanijata {self.company}")
+
+
+
+emp1 = Employee("Tamara", "Front-end developer", "ITQuarks")
+emp2 = Employee("Ivo", "Full Stack developer", "Valtech")
+emp3 = Employee("Adrijana", "Data Scientis", "Technoperia")
+
+employees = [emp1, emp2, emp3]
+
+for e in employees:
+    e.description1()
+
