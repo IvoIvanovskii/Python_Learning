00a80aea3bb1fec3d9fc49c1c823da294f1b20a9
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-06-04 18:59:59 +0200

    Update Classes.py

--- Seventh_Class/Classes.py
+++ Seventh_Class/Classes.py
@@ -6,6 +6,20 @@ class Person:
     def greet(self):
         print(f"Zdravo, jas sum {self.name} i imam {self.age} godini")
 
+    def prezentier(self):
+        print(f"Jas sum {self.name} i imam {self.name}")
+
 p1 = Person("Ivo",22)
 p1.greet()
+
+
+class Student(Person):
+    def __init__(self, name, age, fakultet):
+        super().__init__(name,age)
+        self.fakultet = fakultet
+    
+    def prezentier(self):
+        return super().prezentier()
+        
+
         
